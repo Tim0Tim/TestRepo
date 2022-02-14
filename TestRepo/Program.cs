7f8c3649780f8e47571c4eafa8c1b343d0e37b8e
GRADEN BISHOP <4400112995@OTECH.EDU>
2022-02-14 13:26:03 -0700

    update

--- TestRepo/Program.cs
+++ TestRepo/Program.cs
@@ -4,9 +4,38 @@ namespace TestRepo
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int num1;
+            int num2;
+            int sum;
+            int mult;
+            int sub;
+            int div;
+            int rem;
+
+
+
+
+            Console.Write("Enter first integer: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second integer: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            sum = num1 + num2;
+
+            Console.WriteLine($"{num1} + {num2} = {sum}");
+
+            sub = num1 - num2;
+            Console.WriteLine($"{num1} - {num2} = {sub}");
+
+            mult = num1 * num2;
+            Console.WriteLine($"{num1} x {num2} = {mult}");
+
+            div = num1 / num2;
+            rem = num1 % num2;
+            Console.WriteLine($"{num1} divided by {num2} = {div} with {rem} remaining");
         }
     }
 }
